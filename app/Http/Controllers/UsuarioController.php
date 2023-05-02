8c5e04cc4315072a6eda0987fd6399cce5a413ba
lauguerrero <lauguera2002@gmail.com>
2023-05-02 17:12:52 +0200

    perfil

---
+++ app/Http/Controllers/UsuarioController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Articulo;
+use App\Models\User;
+use App\Models\Deseo;
+
+class UsuarioController extends Controller
+{
+    public function perfil(){
+        
+    }
+}
