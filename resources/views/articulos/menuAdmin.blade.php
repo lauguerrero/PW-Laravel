150dd0ea82cbd60adb2e32cf87a07c32638d8731
lauguerrero <lauguera2002@gmail.com>
2023-05-03 11:44:14 +0200

    creacion menuAdmin

---
+++ resources/views/articulos/menuAdmin.blade.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="es">
+
+<head>
+    <title>TheReUseShop_Admin</title>
+
+    <link rel="stylesheet" href="{{ URL::asset('css/Home.css'); }}">
+</head>
+
+<body>
+    <header>
+        <h1>MENU DE ADMIN</h1>
+        <h2>Este es el menu de administrador del sistema</h2>
+
+        <a href="ListaUsu.php" class="btn1">Lista de Usuarios</a>
+        <a href="ListaAdd.php" class="btn1">Lista de Articulos</a>
+    </header>
+</body>
+
+</html>
