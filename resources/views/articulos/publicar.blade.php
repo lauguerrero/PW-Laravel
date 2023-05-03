a65ffd22cef1a3db9371adef6987be3e207043d9
Daniel <danijgg2002@gmail.com>
2023-05-03 12:54:48 +0200

    Publicar funciona
    Funciona to perfe

--- resources/views/articulos/publicar.blade.php
+++ resources/views/articulos/publicar.blade.php
@@ -13,13 +13,13 @@
 
         @csrf
 
-        <p>Nombre del Articulo: <input type = "text" name = "nombre" size = "40"></p>
-        @if ($errors->has('nombre'))
-                    <div class="alert alert-danger">{{ $errors->first('nombre') }}</div>
+        <p>Nombre del Articulo: <input type = "text" name = "Nombre" size = "40"></p>
+        @if ($errors->has('Nombre'))
+                    <div class="alert alert-danger">{{ $errors->first('Nombre') }}</div>
         @endif
         
         Tematica:
-        <select name="tematica">
+        <select name="Tematica">
             <option>Deporte y ocio</option>
             <option>Electronica</option>
             <option>Moda y accesorios</option>
@@ -28,35 +28,38 @@
             <option>Coleccionismo</option> 
             <option>Otros</option> 
         </select>
+        @if ($errors->has('Tematica'))
+                    <div class="alert alert-danger">{{ $errors->first('Tematica') }}</div>
+        @endif
 
-        <p>Precio: <input type = "number" name = "precio"></p>
-        @if ($errors->has('precio'))
-                    <div class="alert alert-danger">{{ $errors->first('precio') }}</div>
+        <p>Precio: <input type = "number" name = "Precio"></p>
+        @if ($errors->has('Precio'))
+                    <div class="alert alert-danger">{{ $errors->first('Precio') }}</div>
         @endif
         
         <!--<p>Descripcion: <input type = "text" name = "descripcion" size = "50" maxlength = "200"></p>-->
         <p>Descripcion:</p>
-        <textarea name="descripcion" id="" size ="100" maxlength="300"></textarea>
+        <textarea name="Descripcion" id="" size ="100" maxlength="300"></textarea>
         <br>
-        @if ($errors->has('descripcion'))
-                    <div class="alert alert-danger">{{ $errors->first('descripcion') }}</div>
+        @if ($errors->has('Descripcion'))
+                    <div class="alert alert-danger">{{ $errors->first('Descripcion') }}</div>
         @endif
 
         Estado:
-        <select name="estado">
+        <select name="Estado">
             <option>Malo</option>
             <option>Aceptable</option>
             <option>Bueno</option>
             <option>Como Nuevo</option>
         </select>
-        @if ($errors->has('estado'))
-                    <div class="alert alert-danger">{{ $errors->first('estado') }}</div>
+        @if ($errors->has('Estado'))
+                    <div class="alert alert-danger">{{ $errors->first('Estado') }}</div>
         @endif
 
         <!--<p>Imagen: <input type = "text" name = "imagen"></p>-->
-        <p>Imagen: <input type = "file" name = "imagen"></p>
-        @if ($errors->has('imagen'))
-                    <div class="alert alert-danger">{{ $errors->first('imagen') }}</div>
+        <p>Imagen: <input type = "file" name = "Imagen"></p>
+        @if ($errors->has('Imagen'))
+                    <div class="alert alert-danger">{{ $errors->first('Imagen') }}</div>
         @endif
 
         <input type = "submit" value = "Enviar">
